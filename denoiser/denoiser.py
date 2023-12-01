ab9327f444417d6322dcffeed4370b73005ac929
Leslie Ibarra <ibarraleslie16@gmail.com>
2023-12-01 01:04:03 -0700

    edit command args, impl lms

--- denoiser/denoiser.py
+++ denoiser/denoiser.py
@@ -1,22 +1,12 @@
 import argparse
 import numpy as np
 import queue
-from dataclasses import dataclass, field
-from scipy.fft import fft
+import wave
+import librosa 
 from audio_source import WaveFileAudioSource
 from denoiser_preprocessing import DenoiserPreprocessing
-from synthetic_signal_gen import SyntheticSignalGenerator
 import plot_gen
 
-
-@dataclass
-class ExtractedFeatures:
-    '''Data class to store extracted features'''
-    pitch: float = 0
-    band_energies: np.ndarray = field(default_factory=np.array)         # 24 band energies
-    bfcc: np.ndarray = field(default_factory=np.array)                  # 24 BFCCs
-    bfcc_first_derivs: np.ndarray = field(default_factory=np.array)     # First 6 BFCCs
-    bfcc_second_derivs: np.ndarray = field(default_factory=np.array)    # First 6 BFCCs
     
 class Denoiser():
     def __init__(self):
@@ -25,78 +15,89 @@ class Denoiser():
     def split_chunk(self, chunk: np.ndarray) -> np.ndarray:
         return np.split(chunk, self.denoise_preprocessor.chunk_size / self.denoise_preprocessor.window_size)
     
-    def extract_features(self, window):
-        '''Extract the features from the window'''
-        pitch = self.denoise_preprocessor.detect_pitch(window)
+    def denoise_lms(self, chunk: np.ndarray) -> np.ndarray:
+        split_chunk = self.split_chunk(chunk)
         
-        windowed = self.denoise_preprocessor.apply_vorbis_window(window)
-        windowed_fft = fft(windowed)
-        band_energies = self.denoise_preprocessor.compute_band_energies(windowed_fft)
-        bfcc = self.denoise_preprocessor.compute_bfcc(windowed_fft)
-        derivs = self.denoise_preprocessor.compute_bcff_temporal_derivs(bfcc)
+        denoised_chunk = []
         
-        print(f"Pitch: {pitch}, \
-              Band Energies: {band_energies}, \
-              BFCC: {bfcc}, \
-              BFCC First Derivatives: {derivs[0]}, \
-              BFCC Second Derivatives: {derivs[1]}")
-        # return ExtractedFeatures(pitch, bfcc, band_energies)
-        
-    def analyze_windows(self, split_chunk: np.ndarray):
-        features = []
-        
-        for i, window in enumerate(split_chunk):
-            self.extract_features(window)
+        for window in split_chunk:
+            window_stft = librosa.stft(window)
             
-        return features
+            estimated_noise = np.median(window_stft, axis=1)
+            
+            noise_subtracted = np.abs(window_stft - estimated_noise[:, np.newaxis])
+            
+            denoised_window = librosa.istft(noise_subtracted)
+            denoised_chunk.append(denoised_window)
+            
+        return np.concatenate(denoised_chunk)
         
-def main(input_path, output_path):
+    def denoise_rnn(self, chunk: np.ndarray) -> np.ndarray:
+        split_chunk = self.split_chunk(chunk)
+        
+        denoised_chunk = []
+        for window in split_chunk:
+            features = self.analyze_windows(split_chunk)
+            
+            print(f'Window: {features}')
+            
+            # Do the RNN stuff here
+            
+        return np.concatenate(denoised_chunk)
+    
+    # def iterpolate_band_gains(self, band_gains):
+    
+    # def apply_pitch_filter(self, band_filter_coeff):
+        
+def main(input_path, output_path, algorithm, plot):
+    # Sine wave signal
+    # t = np.arange(0, 0.48, 1/denoiser.denoise_preprocessor.sample_rate)
+    # envelope = lambda t: np.exp(-t)
+    # sin_signal = envelope(t) * np.sin(2 * np.pi * 100 * t)
+    
+    # num_chunks = int(len(sin_signal) / denoiser.denoise_preprocessor.chunk_size)
+    # sin_signal_chunks = np.split(sin_signal, num_chunks)    # Audio queue will already contain chunks
+    
+    # for chunk in sin_signal_chunks:
+    #     split_chunk = denoiser.split_chunk(chunk)
+    #     denoiser.analyze_windows(split_chunk)
+    
     denoiser = Denoiser()
     
-    # Sine wave signal
-    t = np.arange(0, 0.48, 1/denoiser.denoise_preprocessor.sample_rate)
-    envelope = lambda t: np.exp(-t)
-    sin_signal = envelope(t) * np.sin(2 * np.pi * 700 * t)
+    if algorithm == 'lms':
+        denoiser_func = denoiser.denoise_lms
+    else:
+        denoiser_func = denoiser.denoise_rnn
     
-    chunk = denoiser.split_chunk(sin_signal)
-    result = denoiser.analyze_windows(chunk)
+    source = WaveFileAudioSource(input_path)
+
+    audio_queue = queue.Queue()
     
+    source.read_into_queue(audio_queue)
+
+    # Placeholder: Add code to write to output file
+    original = []
+    denoised = []
+    
+    while True:
+        try:
+            chunk = audio_queue.get(block=False)
+            chunk_int16 = np.copy(np.frombuffer(chunk, dtype=np.int16))
+            
+            if len(chunk) < DenoiserPreprocessing.CHUNK_SIZE:
+                chunk_int16 = np.pad(chunk_int16, (0, denoiser.denoise_preprocessor.CHUNK_SIZE - len(chunk_int16)))
+            
+            original.append(chunk_int16)
+            
+            denoised_chunk = denoiser_func(chunk_int16)
+            
+            denoised.append(denoised_chunk)
+            
+        except queue.Empty:
+            # Queue is empty, break out of the loop
+            break
     
 
-    # sig_gen = SyntheticSignalGenerator()
-    # path = sig_gen.synthetic_sin_signal(0.48, 100)
-    # source = WaveFileAudioSource(path, DenoiserPreprocessing.SAMPLE_RATE)
-
-    # audio_queue = queue.Queue()
-    
-    # source.read_into_queue(audio_queue)
-    
-    
-    
-    # counter = 0
-    # while True:
-    #     try:
-    #         counter += 1
-    #         chunk = audio_queue.get(block=False)
-    #         chunk_int16 = np.copy(np.frombuffer(chunk, dtype=np.int16))
-            
-    #         if len(chunk) < DenoiserPreprocessing.CHUNK_SIZE:
-    #             chunk_int16 = np.pad(chunk_int16, (0, DenoiserPreprocessing.CHUNK_SIZE - len(chunk_int16)))
-            
-    #         denoiser.analyze_window(chunk_int16)
-            
-    #     except queue.Empty:
-    #         # Queue is empty, break out of the loop
-    #         break
-  
-    
-    
-    # plot_gen.plot_freq_domain(X, DenoiserPreprocessing.SAMPLE_RATE, len(original), 'Original')
-    # plot_gen.plot_freq_domain(X_denoised, DenoiserPreprocessing.SAMPLE_RATE, len(denoised), 'Denosied')
-    
-    # plot_gen.plot_time_domain(original, DenoiserPreprocessing.SAMPLE_RATE, 'Original')
-    # plot_gen.plot_time_domain(denoised, DenoiserPreprocessing.SAMPLE_RATE, 'Denoised')
-        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Denoiser")
 
@@ -105,6 +106,12 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', required=True,
                         help="Path to output denoised speech")
     
+    parser.add_argument('-a', '--algorithm', default='rnn', choices=['rnn', 'lms'],
+                        help="Denoising algorithm to use (default: rnn)")
+    
+    parser.add_argument('-p', '--plot', action='store_true', default=False, 
+                        help="Plot the results")
+    
     args = parser.parse_args()
     
-    main(args.input, args.output)
+    main(args.input, args.output, args.algorithm, args.plot)
